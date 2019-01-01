088aa95eadb6fe79c1bebe40079ecafad4957570
Cezary K. Wagner <Cezary.Wagner@gmail.com>
2019-01-01 23:51:30 +0100

    added progress meter

--- cw_performance/performance.py
+++ cw_performance/performance.py
@@ -45,10 +45,13 @@ class Speedometer(object):
         :param: start_time: Start time if None it is now.
         """
 
+        if self.start_time is not None:
+            raise RuntimeError('Speedometer is already started.')
+
         if start_time:
             self.start_time = start_time
         else:
-            self.start_time = datetime.datetime.now()
+            self.start_time = datetime.datetime.utcnow()
         self.stop_time = None
         if self.events is None or self.seconds:
             self.events = 0
@@ -73,28 +76,10 @@ class Speedometer(object):
         seconds = delta.days * cls.SECONDS_PER_DAY + delta.seconds + delta.microseconds / 1e6
         return seconds
 
-    def get_speed(self):
-        """
-        Get speed of events/seconds using now time or stop time since last reset.
-        :return speed or None if zero seconds
-        """
-
-        # get total + current speed
-        if self.start_time:
-            now = datetime.datetime.now()
-            delta = now - self.start_time
-            seconds = self._delta_to_seconds(delta)
-            # add previous seconds
-            seconds += self.seconds
-            return self.events / seconds if seconds else None
-        # get total speed
-        else:
-            return self.events / self.seconds if self.seconds else None
-
     def get_events(self):
         """
         Get total events counter since last reset.
-        :return Events counter or None if it has never started.
+        :return: Events counter or None if it has never started.
         """
 
         return self.events
@@ -102,12 +87,12 @@ class Speedometer(object):
     def get_seconds(self):
         """
         Get total seconds counter since last reset.
-        :return Seconds counter or None if it has never started.
+        :return: Seconds counter or None if it has never started.
         """
 
         # get total + current speed
         if self.start_time:
-            now = datetime.datetime.now()
+            now = datetime.datetime.utcnow()
             delta = now - self.start_time
             seconds = self._delta_to_seconds(delta)
             # add previous seconds
@@ -117,16 +102,28 @@ class Speedometer(object):
         else:
             return self.seconds
 
+    def get_speed(self):
+        """
+        Get speed of events/seconds using now time or stop time since last reset.
+        :return: speed or None if zero seconds
+        """
+
+        seconds = self.get_seconds()
+        return self.events / seconds if seconds else None
+
     def stop(self, stop_time=None):
         """
         Stop speed measuring.
-        :param: stop_time: Stop time if None it is now.
+        :param stop_time: Stop time if None it is now.
         """
+
+        if self.start_time is None:
+            raise RuntimeError('Speedometer is not started.')
 
         if stop_time:
             self.stop_time = stop_time
         else:
-            self.stop_time = datetime.datetime.now()
+            self.stop_time = datetime.datetime.utcnow()
         delta = self.stop_time - self.start_time
         seconds = self._delta_to_seconds(delta)
         # add previous seconds
@@ -135,10 +132,159 @@ class Speedometer(object):
         self.start_time = None
         self.stop_time = None
 
-    def reset(self):
-        """ Reset times, events and seconds counters. """
 
+class ProgressMeter(object):
+    """
+    Measure speed of events = events/seconds.
+
+    Progress meter works like that:
+    __init__()
+    start()
+    add_event()
+    get_speed() -> now speed
+    stop()
+    get_speed() -> stop speed
+
+    You can start it again and again:
+    start()
+    add_event()
+    get_speed() -> previous + now speed
+    stop()
+    get_speed() -> previous + stop speed
+
+    Or reset:
+    reset()
+
+    """
+
+    SECONDS_PER_DAY = 24 * 60 * 60
+
+    def __init__(self, total_events):
+        self.total_events = total_events
         self.start_time = None
         self.stop_time = None
         self.events = None
         self.seconds = None
+
+    def start(self, start_time=None):
+        """
+        Start speed measuring.
+        :param start_time: Start time if None it is now.
+        """
+
+        if start_time:
+            self.start_time = start_time
+        else:
+            self.start_time = datetime.datetime.utcnow()
+        self.stop_time = None
+        if self.events is None or self.seconds:
+            self.events = 0
+            self.seconds = 0.0
+
+    def add_event(self):
+        """ Add event. """
+
+        if not self.start_time:
+            raise RuntimeError('ProgressMeter is not started.')
+        self.events += 1
+
+    def add_events(self, events):
+        """ Add events. """
+
+        if not self.start_time:
+            raise RuntimeError('ProgressMeter is not started.')
+        self.events += events
+
+    @classmethod
+    def _delta_to_seconds(cls, delta):
+        seconds = delta.days * cls.SECONDS_PER_DAY + delta.seconds + delta.microseconds / 1e6
+        return seconds
+
+    def get_events(self):
+        """
+        Get total events counter since last reset.
+        :return: Events counter or None if it has never started.
+        """
+
+        return self.events
+
+    def get_seconds(self):
+        """
+        Get total seconds counter since last reset.
+        :return: Seconds counter or None if it has never started.
+        """
+
+        # get total + current speed
+        if self.start_time:
+            now = datetime.datetime.utcnow()
+            delta = now - self.start_time
+            seconds = self._delta_to_seconds(delta)
+            # add previous seconds
+            seconds += self.seconds
+            return seconds
+        # get total speed
+        else:
+            return self.seconds
+
+    def get_speed(self):
+        """
+        Get speed of events/seconds using now time or stop time since last reset.
+        :return: speed or None if zero seconds
+        """
+
+        seconds = self.get_seconds()
+        return self.events / seconds if seconds else None
+
+    def get_progress(self):
+        """
+        Get progress.
+        :return: Progress.
+        """
+
+        return self.events / float(self.total_events)
+
+    def get_seconds_left(self):
+        """
+        Get seconds left to end.
+        :return: Predicted seconds left to end or None.
+        """
+
+        speed = self.get_speed()
+        if speed is None:
+            return None
+
+        events_left = self.total_events - self.events
+        seconds_to_end = events_left / speed
+
+        return seconds_to_end
+
+    def get_statistics(self):
+        """
+        Get statistics.
+        :return: Statistics string.
+        """
+
+        return ('done %s of %s, speed %s, predicted end %s',
+                self.events,
+                self.total_events,
+                self.get_speed(),
+                self.get_seconds_left())
+
+    def stop(self, stop_time=None):
+        """
+        Stop speed measuring.
+        :param stop_time: Stop time if None it is now.
+        """
+
+        if stop_time:
+            self.stop_time = stop_time
+        else:
+            self.stop_time = datetime.datetime.utcnow()
+        delta = self.stop_time - self.start_time
+        seconds = self._delta_to_seconds(delta)
+        # add previous seconds
+        self.seconds += seconds + self.seconds
+        # reset times
+        self.start_time = None
+        self.stop_time = None
+
--- tests/cw_performance/test_performance.py
+++ tests/cw_performance/test_performance.py
@@ -8,36 +8,59 @@ import unittest
 
 import time
 
-from cw_performance.performance import Speedometer
+from cw_performance.performance import Speedometer, ProgressMeter
 
 
 class TestSpeedometer(unittest.TestCase):
     def test_basic_flow(self):
         s = Speedometer()
+        # first start
         s.start()
+        s.get_speed()
+        time.sleep(0.01)
         s.add_event()
         s.add_events(2)
+        s.get_speed()
+
+        # first stop
         s.stop()
         s.get_speed()
         s.get_events()
         s.get_seconds()
 
-        s.reset()
+        # second start
         s.start()
+        s.get_speed()
+        time.sleep(0.01)
         s.add_event()
         s.add_events(2)
+        s.get_speed()
+
+        # second stop
         s.stop()
         s.get_speed()
         s.get_events()
         s.get_seconds()
 
-    def test_is_not_started(self):
+    def test_is_not_started_add_event(self):
         s = Speedometer()
         with self.assertRaises(RuntimeError):
             s.add_event()
 
         with self.assertRaises(RuntimeError):
             s.add_events(2)
+
+    def test_is_not_started_stop(self):
+        s = Speedometer()
+        with self.assertRaises(RuntimeError):
+            s.stop()
+
+    def test_is_already_stopped(self):
+        s = Speedometer()
+        s.start()
+        s.stop()
+        with self.assertRaises(RuntimeError):
+            s.stop()
 
     def test_events_counter(self):
         s = Speedometer()
@@ -51,14 +74,76 @@ class TestSpeedometer(unittest.TestCase):
         s.stop()
         self.assertEqual(s.get_events(), 3)
 
-    def test_seconds_counter(self):
-        s = Speedometer()
-        self.assertEqual(s.get_seconds(), None)
+
+class TestProgressMeter(unittest.TestCase):
+    def test_basic_flow(self):
+        s = ProgressMeter(total_events=100)
+
+        # first start
         s.start()
+        s.get_progress()
+        s.get_seconds_left()
+        s.get_speed()
         time.sleep(0.01)
-        self.assertGreaterEqual(s.get_seconds(), 0.01)
+        s.add_event()
+        s.add_events(2)
+        s.get_speed()
+
+        # first stop
         s.stop()
-        t = s.get_seconds()
-        self.assertGreaterEqual(s.get_seconds(), 0.01)
-        self.assertEqual(s.get_seconds(), t)
+        s.get_progress()
+        s.get_seconds_left()
+        s.get_speed()
+        s.get_events()
+        s.get_seconds()
+
+        # second start
+        s.start()
+        s.get_progress()
+        s.get_seconds_left()
+        s.get_speed()
+        time.sleep(0.01)
+        s.add_event()
+        s.add_events(2)
+        s.get_speed()
+
+        # second stop
+        s.stop()
+        s.get_progress()
+        s.get_seconds_left()
+        s.get_speed()
+        s.get_events()
+        s.get_seconds()
+
+    def test_is_not_started_add_event(self):
+        s = Speedometer()
+        with self.assertRaises(RuntimeError):
+            s.add_event()
+
+        with self.assertRaises(RuntimeError):
+            s.add_events(2)
+
+    def test_is_not_started_stop(self):
+        s = Speedometer()
+        with self.assertRaises(RuntimeError):
+            s.stop()
+
+    def test_is_already_stopped(self):
+        s = Speedometer()
+        s.start()
+        s.stop()
+        with self.assertRaises(RuntimeError):
+            s.stop()
+
+    def test_events_counter(self):
+        s = ProgressMeter(total_events=100)
+        self.assertEqual(s.get_events(), None)
+        s.start()
+        self.assertEqual(s.get_events(), 0)
+        s.add_event()
+        self.assertEqual(s.get_events(), 1)
+        s.add_events(2)
+        self.assertEqual(s.get_events(), 3)
+        s.stop()
+        self.assertEqual(s.get_events(), 3)
 
