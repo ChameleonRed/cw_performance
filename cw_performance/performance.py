0706df246c5fe7164984c2ca214fa617b97e6786
Cezary K. Wagner <Cezary.Wagner@gmail.com>
2019-01-02 01:04:01 +0100

    improved statistics string

--- cw_performance/performance.py
+++ cw_performance/performance.py
@@ -264,11 +264,13 @@ class ProgressMeter(object):
         :return: Statistics string.
         """
 
+        seconds_left = self.get_seconds_left()
+
         return ('done %s of %s, speed %s, time left %s',
                 self.events,
                 self.total_events,
                 self.get_speed(),
-                datetime.timedelta(seconds=self.get_seconds_left()))
+                datetime.timedelta(seconds=seconds_left) if seconds_left else None)
 
     def stop(self, stop_time=None):
         """
